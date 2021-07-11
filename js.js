d202ac5d3564e29c2f731e81f1c1da8b1cbcd20f
Ivan <temirbaev-96@mail.ru>
2021-07-11 13:32:52 +0600

    add js

---
+++ js.js
@@ -0,0 +1,41 @@
+let money="Ваш бюджет на месяц?";
+let time="Введите дату в формате YYYY-MM-DD";
+
+console.log(money);
+console.log(time);
+
+let appDate ={
+    money: 25000,
+    time: 10,
+    expenses: 15000,
+    optionalExpenses: '',
+    income:'',
+    savings:false
+
+};
+console.log(appDate["money"]);
+
+
+let arr = ['money','time','expenses']
+console.log (arr[1]);
+
+let rashod= "1.Введите обязательную статью расходов в этом месяце";
+let skolko= "2.Во сколько обойдется?";
+
+console.log(rashod);
+console.log(skolko);
+
+let expenses ={
+    rashod: 10000,
+    skolko: 25000,
+
+};
+
+console.log(expenses["rashod"]);
+console.log(expenses["skolko"]);
+
+
+let zat= "Сколько я трачу в месяц?";
+
+alert(zat);
+alert("500");
